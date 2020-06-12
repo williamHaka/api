a5bb34ee772faa6cd03c32fa0c172f65bc54f5c8
SebastianHaka <63003506+SebastianHaka@users.noreply.github.com>
2020-06-12 17:22:55 -0400

    administrador entity/controller/dao listos y funcionando

---
+++ src/main/java/com/hakalab/api/controller/OrganizacionRestController.java
@@ -0,0 +1,72 @@
+package com.hakalab.api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hakalab.api.entity.Organizacion;
+import com.hakalab.api.service.OrganizacionService;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping(value = "/hakalab")
+public class OrganizacionRestController {
+	@Autowired
+	private OrganizacionService organizacionService;
+	
+	@PostMapping(value = "/organizacion", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<String> addProject(@RequestBody Organizacion organizacion) {
+		Organizacion organizacionSave = organizacionService.save(organizacion);
+		if(organizacionSave == null) {
+			return new ResponseEntity<String>("El rut ingresado ya se encuentra en el sistema: " + organizacion.getRutOrganizacion(), HttpStatus.CONFLICT);
+		}
+		return ResponseEntity.status(HttpStatus.CREATED).body(organizacionSave.toString());
+	}
+	
+	@GetMapping(value = "/organizaciones", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<String> findAll(){
+		List<Organizacion> organizaciones = organizacionService.getAll();
+		if (organizaciones.isEmpty()) {
+			return new ResponseEntity<String>("No hay organizaciones en el sistema", HttpStatus.NOT_FOUND);
+		}
+		return ResponseEntity.status(HttpStatus.OK).body(organizaciones.toString());
+	}
+	
+	@GetMapping(value = "/organizacion", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<String> getOrganizacion(@RequestBody Organizacion organizacion) {
+		Organizacion organizacionExist = organizacionService.getById(organizacion.getIdOrganizacion());
+		if (organizacionExist == null) {
+			return new ResponseEntity<String>("No se ha encontrado una organizacion con id: " + organizacion.getIdOrganizacion(), HttpStatus.NOT_FOUND);
+		}
+		return ResponseEntity.status(HttpStatus.OK).body(organizacionExist.toString());
+	}
+	
+	@PutMapping(value = "/organizacion", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<String> updateProject(@RequestBody Organizacion organizacion) {
+		Organizacion organizacionUpdate = organizacionService.update(organizacion);
+		if(organizacionUpdate == null) {
+			return new ResponseEntity<String>("No se encuentra organizacion: " + organizacion.getNombreOrganizacion() + " en el sistema", HttpStatus.NOT_FOUND);
+		}
+		return ResponseEntity.status(HttpStatus.OK).body(organizacionUpdate.toString());
+	}
+	
+	@DeleteMapping(value = "/organizacion", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<String> deleteProject(@RequestBody Organizacion organizacion) {
+		Integer status = organizacionService.detele(organizacion.getIdOrganizacion());
+		if(status == 0) {
+			return new ResponseEntity<String>("No se ha encontrado la organizacion: "+organizacion.getNombreOrganizacion(), HttpStatus.NOT_FOUND);
+		}
+		return ResponseEntity.status(HttpStatus.OK).body("Organizacion eliminada: "+organizacion.getNombreOrganizacion());
+	}
+}
---
+++ src/main/java/com/hakalab/api/entity/Organizacion.java
@@ -0,0 +1,85 @@
+package com.hakalab.api.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "organizacion")
+public class Organizacion {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Integer idOrganizacion;
+	
+	@Column(name = "nombre")
+	private String nombreOrganizacion;
+	
+	@Column(name = "rut")
+	private String rutOrganizacion;
+	
+	@Column(name = "direccion")
+	private String direccionOrganizacion;
+	
+	public Integer getIdOrganizacion() {
+		return idOrganizacion;
+	}
+
+
+
+	public void setIdOrganizacion(Integer idOrganizacion) {
+		this.idOrganizacion = idOrganizacion;
+	}
+
+
+
+	public String getNombreOrganizacion() {
+		return nombreOrganizacion;
+	}
+
+
+
+	public void setNombreOrganizacion(String nombreOrganizacion) {
+		this.nombreOrganizacion = nombreOrganizacion;
+	}
+
+
+
+	public String getRutOrganizacion() {
+		return rutOrganizacion;
+	}
+
+
+
+	public void setRutOrganizacion(String rutOrganizacion) {
+		this.rutOrganizacion = rutOrganizacion;
+	}
+
+
+
+	public String getDireccionOrganizacion() {
+		return direccionOrganizacion;
+	}
+
+
+
+	public void setDireccionOrganizacion(String direccionOrganizacion) {
+		this.direccionOrganizacion = direccionOrganizacion;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "{"
+				+"\r\n \"idOrganizacion\": "+idOrganizacion+","
+				+"\r\n \"nombreOrganizacion\": \""+nombreOrganizacion+"\","
+				+"\r\n \"rutOrganizacion\": \""+rutOrganizacion+"\","
+				+"\r\n \"direccionOrganizacion\": \""+direccionOrganizacion+"\""
+				+"\n}";
+	}
+}
